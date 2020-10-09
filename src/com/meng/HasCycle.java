f8359540bad34758e168f1570fee1bbab0669ebd
root <root@qq.com>
2020-10-09 21:44:53 +0800

    141. 环形链表

---
+++ src/com/meng/HasCycle.java
@@ -0,0 +1,69 @@
+package com.meng;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class HasCycle {
+    //将已经遍历过的节点放到set中若set中已存在则返回true
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null){
+            if (set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+    //官方解法
+
+    /**
+     * 哈希表
+     * @param head
+     * @return
+     */
+    public boolean hasCycle1(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    /**
+     * 快慢指针
+     * @param head
+     * @return
+     */
+        public boolean hasCycle2(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+
+}
