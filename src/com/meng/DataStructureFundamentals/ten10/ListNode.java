b5a1bea5776277dc4779c8f5b13f628c4e4767e3
孟松 <mengsong@58.com>
2022-03-30 20:25:38 +0800

    2. 两数相加(中等)

--- src/com/meng/DataStructureFundamentals/ten10/ListNode.java
+++ src/com/meng/DataStructureFundamentals/ten10/ListNode.java
@@ -3,8 +3,10 @@ package com.meng.DataStructureFundamentals.ten10;
 public class ListNode {
     int val;
     ListNode next;
+    ListNode() {}
     ListNode(int x) {
         val = x;
         next = null;
     }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
