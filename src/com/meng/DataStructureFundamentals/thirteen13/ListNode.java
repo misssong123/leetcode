c3369fc0f778ada65d416fbd5f9e0a166e689398
孟松 <mengsong@58.com>
2022-04-02 16:17:22 +0800

    143. 重排链表(中等)

---
+++ src/com/meng/DataStructureFundamentals/thirteen13/ListNode.java
@@ -0,0 +1,12 @@
+package com.meng.DataStructureFundamentals.thirteen13;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
