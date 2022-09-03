eeb35d86ea24b09d94ffa28ce12220a78ff97d96
孟松 <mengsong@58.com>
2022-09-03 16:18:50 +0800

    176. 第二高的薪水(中等)

---
+++ src/com/meng/enterprise/day02/sql176.sql
@@ -0,0 +1,70 @@
+/**
+  176. 第二高的薪水(中等)
+SQL架构
+Employee 表：
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id 是这个表的主键。
+表的每一行包含员工的工资信息。
+
+
+编写一个 SQL 查询，获取并返回 Employee 表中第二高的薪水 。如果不存在第二高的薪水，查询应该返回 null 。
+
+查询结果如下例所示。
+
+
+
+示例 1：
+
+输入：
+Employee 表：
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+输出：
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+示例 2：
+
+输入：
+Employee 表：
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+输出：
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+ */
+
+ /**
+   sql
+  */
+SELECT
+    (SELECT DISTINCT
+         Salary
+     FROM
+         Employee
+     ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+;
+
+
+select ifnull( (
+                   select distinct salary salary  from Employee order by salary desc limit 1,1
+           ),null) as SecondHighestSalary
