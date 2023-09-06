c602570c18ca2449e4e18de61672e1dc5717fa61
孟松 <mengsong@58.com>
2023-09-06 10:31:00 +0800

    2023-09-06:[1123]最深叶节点的最近公共祖先.md-[medium]

--- src/com/meng/route/leetcode/editor/cn/LowestCommonAncestorOfDeepestLeaves1123.java
+++ src/com/meng/route/leetcode/editor/cn/LowestCommonAncestorOfDeepestLeaves1123.java
@@ -1,27 +1,9 @@
-package com.meng.route.leetcode.editor.cn;//leetcode submit region begin(Prohibit modification and deletion)
-
-
+package com.meng.route.leetcode.editor.cn;
 
 import com.meng.oneQuestionPerDay.leetcode.editor.cn.util.TreeNode;
 import javafx.util.Pair;
 
 import java.util.*;
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class SolutionLcaDeepestLeaves1123 {
     /**
      * 解答成功:
@@ -107,4 +89,4 @@ class SolutionLcaDeepestLeaves1123 {
     }
 
 }
-//leetcode submit region end(Prohibit modification and deletion)
+
