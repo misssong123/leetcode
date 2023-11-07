79c7ad06ba43f7de91a9bdcdd938c3446c52f652
孟松 <mengsong@58.com>
2023-11-07 09:52:24 +0800

    2023-11-05:[2586]统计范围内的元音字符串数.md-[easy]

---
+++ src/com/meng/oneQuestionPerDay/leetcode/editor/cn/CountTheNumberOfVowelStringsInRange2586.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    static Set<Character> cache = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+
+    /**
+     * 解答成功:
+     * 	执行耗时:1 ms,击败了100.00% 的Java用户
+     * 	内存消耗:41.9 MB,击败了76.39% 的Java用户
+     * @param words
+     * @param left
+     * @param right
+     * @return
+     */
+    public int vowelStringsMy(String[] words, int left, int right) {
+        int res = 0;
+        for(int i = left ; i <= right ; i++){
+            if (cache.contains(words[i].charAt(0))&&cache.contains(words[i].charAt(words[i].length()-1))){
+                res++;
+            }
+        }
+        return res;
+    }
+
+    /**
+     *解答成功:
+     * 	执行耗时:2 ms,击败了31.42% 的Java用户
+     * 	内存消耗:41.9 MB,击败了78.03% 的Java用户
+     * @param words
+     * @param left
+     * @param right
+     * @return
+     */
+    public int vowelStrings(String[] words, int left, int right) {
+        Set<Character> vowels = new HashSet<Character>() {{
+            add('a');
+            add('e');
+            add('i');
+            add('o');
+            add('u');
+        }};
+        int ans = 0;
+        for (int i = left; i <= right; ++i) {
+            String word = words[i];
+            if (vowels.contains(word.charAt(0)) && vowels.contains(word.charAt(word.length() - 1))) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+
+}
