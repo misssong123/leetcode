be4444b648a97af8d6a22ddc3e6e71a052dd3f4b
孟松 <mengsong@58.com>
2023-12-21 20:03:28 +0800

    2023-12-21:[2866]美丽塔.md-[medium]

---
+++ src/com/meng/oneQuestionPerDay/leetcode/editor/cn/MaximumSumOfHeights2866.java
@@ -0,0 +1,97 @@
+package com.meng.oneQuestionPerDay.leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MaximumSumOfHeights2866 {
+    public long maximumSumOfHeightsMy(List<Integer> maxHeights) {
+        return -1;
+    }
+
+    /**
+     * 解答成功:
+     * 	执行耗时:38 ms,击败了45.43% 的Java用户
+     * 	内存消耗:65.3 MB,击败了31.37% 的Java用户
+     * @param maxHeights
+     * @return
+     */
+    public long maximumSumOfHeights1(List<Integer> maxHeights) {
+        int n = maxHeights.size();
+        long res = 0;
+        long[] prefix = new long[n];
+        long[] suffix = new long[n];
+        Deque<Integer> stack1 = new ArrayDeque<Integer>();
+        Deque<Integer> stack2 = new ArrayDeque<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack1.isEmpty() && maxHeights.get(i) < maxHeights.get(stack1.peek())) {
+                stack1.pop();
+            }
+            if (stack1.isEmpty()) {
+                prefix[i] = (long) (i + 1) * maxHeights.get(i);
+            } else {
+                prefix[i] = prefix[stack1.peek()] + (long) (i - stack1.peek()) * maxHeights.get(i);
+            }
+            stack1.push(i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack2.isEmpty() && maxHeights.get(i) < maxHeights.get(stack2.peek())) {
+                stack2.pop();
+            }
+            if (stack2.isEmpty()) {
+                suffix[i] = (long) (n - i) * maxHeights.get(i);
+            } else {
+                suffix[i] = suffix[stack2.peek()] + (long) (stack2.peek() - i) * maxHeights.get(i);
+            }
+            stack2.push(i);
+            res = Math.max(res, prefix[i] + suffix[i] - maxHeights.get(i));
+        }
+        return res;
+    }
+
+    /**
+     * 解答成功:
+     * 	执行耗时:30 ms,击败了71.28% 的Java用户
+     * 	内存消耗:62.8 MB,击败了56.91% 的Java用户
+     * @param maxHeights
+     * @return
+     */
+    public long maximumSumOfHeights2(List<Integer> maxHeights) {
+        int[] a = maxHeights.stream().mapToInt(i -> i).toArray();
+        int n = a.length;
+        long[] suf = new long[n + 1];
+        ArrayDeque<Integer> st = new ArrayDeque<Integer>();
+        st.push(n); // 哨兵
+        long sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int x = a[i];
+            while (st.size() > 1 && x <= a[st.peek()]) {
+                int j = st.pop();
+                sum -= (long) a[j] * (st.peek() - j); // 撤销之前加到 sum 中的
+            }
+            sum += (long) x * (st.peek() - i); // 从 i 到 st.peek()-1 都是 x
+            suf[i] = sum;
+            st.push(i);
+        }
+
+        long ans = sum;
+        st.clear();
+        st.push(-1); // 哨兵
+        long pre = 0;
+        for (int i = 0; i < n; i++) {
+            int x = a[i];
+            while (st.size() > 1 && x <= a[st.peek()]) {
+                int j = st.pop();
+                pre -= (long) a[j] * (j - st.peek()); // 撤销之前加到 pre 中的
+            }
+            pre += (long) x * (i - st.peek()); // 从 st.peek()+1 到 i 都是 x
+            ans = Math.max(ans, pre + suf[i + 1]);
+            st.push(i);
+        }
+        return ans;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
