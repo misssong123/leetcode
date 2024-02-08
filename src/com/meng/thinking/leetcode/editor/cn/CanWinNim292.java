e0eaa1a07727cfbcd6faa9cbdc996337dac043c4
孟松 <mengsong@58.com>
2024-02-08 15:38:29 +0800

    2024-02-04:[292]Nim 游戏.md-[easy]

---
+++ src/com/meng/thinking/leetcode/editor/cn/CanWinNim292.java
@@ -0,0 +1,14 @@
+package com.meng.thinking.leetcode.editor.cn;
+
+class CanWinNim292 {
+    /**
+     * 解答成功:
+     * 	执行耗时:0 ms,击败了100.00% 的Java用户
+     * 	内存消耗:39.3 MB,击败了69.03% 的Java用户
+     * @param n
+     * @return
+     */
+    public boolean canWinNim(int n) {
+        return n % 4 !=0;
+    }
+}
