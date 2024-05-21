436f648df43b34dd8d1bf1f764f9fa04332bda8b
孟松 <mengsong@58.com>
2024-05-21 09:55:36 +0800

    2024-05-21:[2769]找出最大的可达成数字.md-[easy]

---
+++ src/com/meng/thinking/leetcode/editor/cn/TheMaximumAchievableX2769.java
@@ -0,0 +1,17 @@
+package com.meng.thinking.leetcode.editor.cn;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class TheMaximumAchievableX2769 {
+    /**
+     * 解答成功:
+     * 	执行耗时:1 ms,击败了100.00% 的Java用户
+     * 	内存消耗:40.5 MB,击败了46.32% 的Java用户
+     * @param num
+     * @param t
+     * @return
+     */
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2*t;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
