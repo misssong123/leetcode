3853cfb8c18f8b906e8d4da81be127725459ca3b
孟松 <mengsong@58.com>
2024-08-13 19:30:15 +0800

    2024-08-13:[122]买卖股票的最佳时机II-第八题-[medium]

---
+++ src/com/meng/interview150/leetcode/editor/cn/Interview008MaxProfit.java
@@ -0,0 +1,22 @@
+package com.meng.interview150.leetcode.editor.cn;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Interview008MaxProfit {
+    /**
+     * 贪心做法
+     * 只要比前一天的结果大，就卖出
+     * 解答成功:
+     * 	执行耗时:1 ms,击败了75.60% 的Java用户
+     * 	内存消耗:44.5 MB,击败了92.09% 的Java用户
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for(int i = 1 ; i < prices.length; i++){
+            res+=Math.max(0,prices[i]-prices[i-1]);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
