108360c133b5b40de228e90d61e669f6c98edaf0
孟松 <mengsong@58.com>
2024-08-28 16:35:01 +0800

    2024-08-28:[226]翻转二叉树-第70题-[easy]

---
+++ src/com/meng/interview150/leetcode/editor/cn/Interview070InvertTree.java
@@ -0,0 +1,36 @@
+package com.meng.interview150.leetcode.editor.cn;//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Interview070InvertTree {
+    /**
+     * 解答成功:
+     * 	执行耗时:0 ms,击败了100.00% 的Java用户
+     * 	内存消耗:40.3 MB,击败了6.20% 的Java用户
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
