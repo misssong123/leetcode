1db9f185bb3503b72d359e629b8e936ecd00aa54
孟松 <mengsong@58.com>
2024-10-02 20:03:13 +0800

    2024-10-02:[190] 颠倒二进制位 -第127题-[easy]

---
+++ src/com/meng/interview150/leetcode/editor/cn/Interview127ReverseBits.java
@@ -0,0 +1,19 @@
+package com.meng.interview150.leetcode.editor.cn;
+
+public class Interview127ReverseBits {
+    /**
+     * 解答成功:
+     * 	执行耗时:0 ms,击败了100.00% 的Java用户
+     * 	内存消耗:40.9 MB,击败了35.87% 的Java用户
+     * @param n
+     * @return
+     */
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
