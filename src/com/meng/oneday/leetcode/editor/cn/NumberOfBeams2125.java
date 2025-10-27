63bacbe589ea10c3345fb15cd2675cd110ee132c
孟松 <mengsong@58.com>
2025-10-27 10:06:00 +0800

    2025-10-24:[36]-有效的数独-[medium]

---
+++ src/com/meng/oneday/leetcode/editor/cn/NumberOfBeams2125.java
@@ -0,0 +1,50 @@
+package com.meng.oneday.leetcode.editor.cn;
+
+class NumberOfBeams2125 {
+    /**
+     * 解答成功:
+     * 	执行耗时:13 ms,击败了26.44% 的Java用户
+     * 	内存消耗:44.4 MB,击败了14.94% 的Java用户
+     * @param bank
+     * @return
+     */
+    public int numberOfBeams2125(String[] bank) {
+        int res = 0,pre = 0,num = 0;
+        for (String s : bank) {
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '1') {
+                    num++;
+                    res += pre;
+                }
+            }
+            if (num != 0) {
+                pre = num;
+                num = 0;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 解答成功:
+     * 	执行耗时:10 ms,击败了91.95% 的Java用户
+     * 	内存消耗:44.4 MB,击败了14.94% 的Java用户
+     * @param bank
+     * @return
+     */
+    public int numberOfBeams(String[] bank) {
+        int ans = 0;
+        int preCnt = 0;
+        for (String row : bank) {
+            int cnt = 0;
+            for (char ch : row.toCharArray()) {
+                cnt += ch - '0';
+            }
+            if (cnt > 0) {
+                ans += preCnt * cnt;
+                preCnt = cnt;
+            }
+        }
+        return ans;
+    }
+}
