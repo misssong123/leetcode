4c8d43356c9bcd33b3b41dcd67e0eafc0126450b
孟松 <mengsong@58.com>
2025-11-10 11:52:28 +0800

    2025-11-10:[LCR 194]-二叉树的最近公共祖先-[medium]

--- src/com/meng/top100/leetcode/editor/cn/LowestCommonAncestorLCR194.java
+++ src/com/meng/top100/leetcode/editor/cn/LowestCommonAncestorLCR194.java
@@ -60,12 +60,12 @@ class LowestCommonAncestorLCR194 {
      * @param q
      * @return
      */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestorOther(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root; // 找到 p 或 q 就不往下递归了，原因见上面答疑
         }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        TreeNode left = lowestCommonAncestorOther(root.left, p, q);
+        TreeNode right = lowestCommonAncestorOther(root.right, p, q);
         if (left != null && right != null) { // 左右都找到
             return root; // 当前节点是最近公共祖先
         }
@@ -73,5 +73,16 @@ class LowestCommonAncestorLCR194 {
         // 如果只有右子树找到，就返回右子树的返回值
         // 如果左右子树都没有找到，就返回 null（注意此时 right = null）
         return left != null ? left : right;
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftNode = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightNode = lowestCommonAncestor(root.right,p,q);
+        if (rightNode != null  && leftNode!= null){
+            return root;
+        }
+        return leftNode != null ? leftNode : rightNode;
     }
 }
