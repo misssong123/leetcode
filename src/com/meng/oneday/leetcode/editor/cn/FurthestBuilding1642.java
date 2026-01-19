21841f39f3d0a8bccfe71cb4ecad22ae6c966314
孟松 <mengsong@58.com>
2026-01-19 17:29:18 +0800

    2026-01-19[1642]-可以到达的最远建筑-[medium]

--- src/com/meng/oneday/leetcode/editor/cn/FurthestBuilding1642.java
+++ src/com/meng/oneday/leetcode/editor/cn/FurthestBuilding1642.java
@@ -1,7 +1,38 @@
+package com.meng.oneday.leetcode.editor.cn;
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-class Solution {
+class FurthestBuilding1642 {
+    /**
+     * 解答成功:
+     * 	执行耗时:20 ms,击败了60.96% 的Java用户
+     * 	内存消耗:72 MB,击败了38.50% 的Java用户
+     * @param heights
+     * @param bricks
+     * @param ladders
+     * @return
+     */
+    public int furthestBuilding1642(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Integer::compareTo);
+        for(int i = 1 ; i < heights.length ; i++){
+            int diff = heights[i] - heights[i-1];
+            if (diff > 0){
+                queue.add(diff);
+                if (queue.size() > ladders){
+                    bricks -= queue.poll();
+                    if (bricks < 0){
+                        return i - 1;
+                    }
+                }
+            }
+        }
+        return heights.length -1;
+    }
+
+
+
+
     int res = 0;
     boolean[][][] visited;
     public int furthestBuildingTimeOut(int[] heights, int bricks, int ladders) {
@@ -42,7 +73,7 @@ class Solution {
      * @param ladders
      * @return
      */
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+    public int furthestBuildingOther(int[] heights, int bricks, int ladders) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         int ans = 0;
         for(int i = 1;i < heights.length;i++){
