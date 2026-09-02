0ca9c48368fee2839bddc7023d4ae406d984d69c
孟松 <mengsong@58.com>
2026-09-02 10:04:06 +0800

    2026-09-02-[3875]-构造奇偶一致的数组I-[easy]

---
+++ src/com/meng/oneday/leetcode/editor/cn/UniformArray3875.java
@@ -0,0 +1,14 @@
+package com.meng.oneday.leetcode.editor.cn;
+
+class UniformArray3875 {
+    /**
+     * 解答成功:
+     * 	执行耗时:0 ms,击败了100.00% 的Java用户
+     * 	内存消耗:44.2 MB,击败了93.85% 的Java用户
+     * @param nums1
+     * @return
+     */
+    public boolean uniformArray3875(int[] nums1) {
+        return true;
+    }
+}
